d89025021aea301b4f4c0de351671f4fa962d8b1
domos4 <domos@op.pl>
2016-05-28 21:14:23 +0200

    freeze() cd.

--- Cloneable.js
+++ Cloneable.js
@@ -2,39 +2,51 @@ var $list2 = $('#list2');
 
 function freeze() {
     $(".tile").remove();
-    $('#list2').show();
+    // $('#list2').show();
+    createBoardFreeze();
 }
 
 function createBoardFreeze() {
-    $('#list2').hide();
+
+    var list = getOrder();
+    for (var i=0; i<list.length; i++) {
+        list[i] = list[i].charCodeAt(0) - 65 + 1;
+        if (list[i] > 9)
+            --list[i];
+    }
+
+    var listID =[];
+
     for (var i = 0; i < numTiles; i++) {
         createTileFreeze(i);
+        listID[i] = '#tileFreeze' + list[i];
     }
-    $('#tileFreeze1').css('transform','translate3d(0px, 0px, 0px)');
-    $('#tileFreeze2').css('transform','translate3d(80.6px, 0px, 0px)');
-    $('#tileFreeze3').css('transform','translate3d(161.2px, 0px, 0px)');
-    $('#tileFreeze4').css('transform','translate3d(241.8px, 0px, 0px)');
-    $('#tileFreeze5').css('transform','translate3d(322.4px, 0px, 0px)');
-    $('#tileFreeze6').css('transform','translate3d(0px, 80.6px, 0px)');
-    $('#tileFreeze7').css('transform','translate3d(80.6px, 80.6px, 0px)');
-    $('#tileFreeze8').css('transform','translate3d(161.2px, 80.6px, 0px)');
-    $('#tileFreeze9').css('transform','translate3d(241.8px, 80.6px, 0px)');
-    $('#tileFreeze10').css('transform','translate3d(322.4px, 80.6px, 0px)');
-    $('#tileFreeze11').css('transform','translate3d(0px, 161.2px, 0px)');
-    $('#tileFreeze12').css('transform','translate3d(80.6px, 161.2px, 0px)');
-    $('#tileFreeze13').css('transform','translate3d(161.2px, 161.2px, 0px)');
-    $('#tileFreeze14').css('transform','translate3d(241.8px, 161.2px, 0px)');
-    $('#tileFreeze15').css('transform','translate3d(322.4px, 161.2px, 0px)');
-    $('#tileFreeze16').css('transform','translate3d(0px, 241.8px, 0px)');
-    $('#tileFreeze17').css('transform','translate3d(80.6px, 241.8px, 0px)');
-    $('#tileFreeze18').css('transform','translate3d(161.2px, 241.8px, 0px)');
-    $('#tileFreeze19').css('transform','translate3d(241.8px, 241.8px, 0px)');
-    $('#tileFreeze20').css('transform','translate3d(322.4px, 241.8px, 0px)');
-    $('#tileFreeze21').css('transform','translate3d(0px, 322.4px, 0px)');
-    $('#tileFreeze22').css('transform','translate3d(80.6px, 322.4px, 0px)');
-    $('#tileFreeze23').css('transform','translate3d(161.2px, 322.4px, 0px)');
-    $('#tileFreeze24').css('transform','translate3d(241.8px, 322.4px, 0px)');
-    $('#tileFreeze25').css('transform','translate3d(322.4px, 322.4px, 0px)');
+
+    $(listID[0]).css('transform','translate3d(0px, 0px, 0px)');
+    $(listID[1]).css('transform','translate3d(80.6px, 0px, 0px)');
+    $(listID[2]).css('transform','translate3d(161.2px, 0px, 0px)');
+    $(listID[3]).css('transform','translate3d(241.8px, 0px, 0px)');
+    $(listID[4]).css('transform','translate3d(322.4px, 0px, 0px)');
+    $(listID[5]).css('transform','translate3d(0px, 80.6px, 0px)');
+    $(listID[6]).css('transform','translate3d(80.6px, 80.6px, 0px)');
+    $(listID[7]).css('transform','translate3d(161.2px, 80.6px, 0px)');
+    $(listID[8]).css('transform','translate3d(241.8px, 80.6px, 0px)');
+    $(listID[9]).css('transform','translate3d(322.4px, 80.6px, 0px)');
+    $(listID[10]).css('transform','translate3d(0px, 161.2px, 0px)');
+    $(listID[11]).css('transform','translate3d(80.6px, 161.2px, 0px)');
+    $(listID[12]).css('transform','translate3d(161.2px, 161.2px, 0px)');
+    $(listID[13]).css('transform','translate3d(241.8px, 161.2px, 0px)');
+    $(listID[14]).css('transform','translate3d(322.4px, 161.2px, 0px)');
+    $(listID[15]).css('transform','translate3d(0px, 241.8px, 0px)');
+    $(listID[16]).css('transform','translate3d(80.6px, 241.8px, 0px)');
+    $(listID[17]).css('transform','translate3d(161.2px, 241.8px, 0px)');
+    $(listID[18]).css('transform','translate3d(241.8px, 241.8px, 0px)');
+    $(listID[19]).css('transform','translate3d(322.4px, 241.8px, 0px)');
+    $(listID[20]).css('transform','translate3d(0px, 322.4px, 0px)');
+    $(listID[21]).css('transform','translate3d(80.6px, 322.4px, 0px)');
+    $(listID[22]).css('transform','translate3d(161.2px, 322.4px, 0px)');
+    $(listID[23]).css('transform','translate3d(241.8px, 322.4px, 0px)');
+    $(listID[24]).css('transform','translate3d(322.4px, 322.4px, 0px)');
 }
 
 function createTileFreeze(i) {
